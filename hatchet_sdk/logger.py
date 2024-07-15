467905a818a01820b590d555cd82bdd2304a737a
Alexander Belanger <alexander@hatchet.run>
2024-07-15 16:45:45 -0400

    chore: lint

--- hatchet_sdk/logger.py
+++ hatchet_sdk/logger.py
@@ -1,13 +1,11 @@
-import sys
 import logging
+import sys
 
 # logging config
 logging.basicConfig(
     level=logging.ERROR,
-    format='[%(levelname)s] hatchet -- %(asctime)s - %(message)s',
-    handlers=[
-        logging.StreamHandler(sys.stdout)
-    ]
+    format="[%(levelname)s] hatchet -- %(asctime)s - %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)],
 )
 
 logger = logging.getLogger()
