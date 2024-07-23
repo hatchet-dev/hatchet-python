3780fa149bdc48f5f48267207b363f0f9749f1ce
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-07-23 14:16:50 -0400

    add tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -12,20 +12,3 @@ def hatchet():
 
 def test_client(hatchet):
     assert hatchet
-
-
-# requires scope module or higher for shared event loop
-@pytest.mark.asyncio(scope="session")
-async def test_listen(hatchet):
-    run = hatchet.client.admin.get_workflow_run("839e089c-6708-4708-ae79-bad47c832580")
-    result = await run.result()
-    print(result)
-    assert result
-
-
-@pytest.mark.asyncio(scope="session")
-async def test_listen2(hatchet):
-    run = hatchet.client.admin.get_workflow_run("839e089c-6708-4708-ae79-bad47c832580")
-    result = await run.result()
-    print(result)
-    assert result
---
+++ tests/test_fanout.py
@@ -0,0 +1,37 @@
+import subprocess
+import time
+from hatchet_sdk import Hatchet
+import pytest
+
+
+@pytest.fixture
+def hatchet():
+    return Hatchet(debug=True)
+
+@pytest.fixture(scope="session", autouse=True)
+def fixture_background_hatchet_worker():
+    proc = subprocess.Popen(["poetry", "run", "python3", "./examples/fanout/worker.py"])
+
+    # sleep long enough to make sure we are up and running
+    # it would be nice to NOT do this, but we need to ensure the worker is running before we trigger any events
+    time.sleep(5)
+
+    yield
+
+    proc.terminate()
+    proc.wait()
+
+# requires scope module or higher for shared event loop
+@pytest.mark.asyncio(scope="session")
+async def test_run(hatchet, ):
+    run = hatchet.client.admin.run_workflow("Parent", {"n": 2})
+    result = await run.result()
+    assert len(result["spawn"]["results"]) == 2
+
+
+# requires scope module or higher for shared event loop
+@pytest.mark.asyncio(scope="session")
+async def test_run2(hatchet):
+    run = hatchet.client.admin.run_workflow("Parent", {"n": 2})
+    result = await run.result()
+    assert len(result["spawn"]["results"]) == 2
