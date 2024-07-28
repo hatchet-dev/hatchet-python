22931d6c992121b0b9531f879d80e45cbed9d65b
gabriel ruttner <gabriel.ruttner@gmail.com>
2024-07-28 07:42:43 -0700

    chore: surface worker error

--- tests/utils/bg_worker.py
+++ tests/utils/bg_worker.py
@@ -1,11 +1,17 @@
 import subprocess
 import time
+from multiprocessing import process
+
 import pytest
+
 
 def fixture_bg_worker(command, startup_time=5):
     @pytest.fixture(scope="session", autouse=True)
     def fixture_background_hatchet_worker():
-        proc = subprocess.Popen(command)
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        if proc.returncode is not None:
+            raise Exception("worker failed %d" % (proc.returncode))
 
         # sleep long enough to make sure we are up and running
         time.sleep(startup_time)
