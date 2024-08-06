ae578c77f12ce05f8c4ecf2926598e0871008894
srhinos <6531393+srhinos@users.noreply.github.com>
2024-08-06 12:05:15 -0400

    Generated OpenAPI Files

--- hatchet_sdk/clients/rest/api/log_api.py
+++ hatchet_sdk/clients/rest/api/log_api.py
@@ -421,9 +421,10 @@ class LogApi:
         # process the body parameter
 
         # set the HTTP header `Accept`
-        _header_params["Accept"] = self.api_client.select_header_accept(
-            ["application/json"]
-        )
+        if "Accept" not in _header_params:
+            _header_params["Accept"] = self.api_client.select_header_accept(
+                ["application/json"]
+            )
 
         # authentication setting
         _auth_settings: List[str] = ["cookieAuth", "bearerAuth"]
