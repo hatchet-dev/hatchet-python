cbe1dd36532f75d6a3e22287b3acb31dd3f23414
Mac Wilkinson <macw713+work@gmail.com>
2024-08-20 15:30:14 -0400

    Expose Workflow Run Step Error in Runner Context (#141)
    * Add Ability to Fetch Workflow Run Step Errors in Step Context
    
    * Update Example / Add Test
    
    * Bump Version Number
    
    ---------
    
    Co-authored-by: srhinos <6531393+srhinos@users.noreply.github.com>

--- examples/on_failure/test_on_failure.py
+++ examples/on_failure/test_on_failure.py
@@ -1,14 +1,32 @@
-# from hatchet_sdk import Hatchet
-# import pytest
+import asyncio
 
-# from tests.utils import fixture_bg_worker
-# from tests.utils.hatchet_client import hatchet_client_fixture
+import pytest
+
+from hatchet_sdk import Hatchet
+from hatchet_sdk.clients.rest.models.job_run_status import JobRunStatus
+from tests.utils import fixture_bg_worker
+from tests.utils.hatchet_client import hatchet_client_fixture
+
+hatchet = hatchet_client_fixture()
+worker = fixture_bg_worker(["poetry", "run", "on_failure"])
 
 
-# hatchet = hatchet_client_fixture()
-# worker = fixture_bg_worker(["poetry", "run", "manual_trigger"])
+# requires scope module or higher for shared event loop
+@pytest.mark.asyncio(scope="session")
+async def test_run_timeout(hatchet: Hatchet):
+    run = hatchet.admin.run_workflow("OnFailureWorkflow", {})
+    try:
+        await run.result()
+        assert False, "Expected workflow to timeout"
+    except Exception as e:
+        assert "step1 failed" in str(e)
 
-# # requires scope module or higher for shared event loop
-# @pytest.mark.asyncio(scope="session")
-# async def test_run(hatchet: Hatchet):
-#     # TODO
+    await asyncio.sleep(2)  # Wait for the on_failure job to finish
+
+    job_runs = hatchet.rest.workflow_run_get(run.workflow_run_id).job_runs
+    assert len(job_runs) == 2
+
+    successful_job_runs = [jr for jr in job_runs if jr.status == JobRunStatus.SUCCEEDED]
+    failed_job_runs = [jr for jr in job_runs if jr.status == JobRunStatus.FAILED]
+    assert len(successful_job_runs) == 1
+    assert len(failed_job_runs) == 1
