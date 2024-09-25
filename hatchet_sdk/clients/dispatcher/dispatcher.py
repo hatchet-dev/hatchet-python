8e48d1b6fff9565623b5d183aa65371954dfe735
abelanger5 <alexander@hatchet.run>
2024-09-24 20:39:48 -0400

    fix: send failure events when we cannot send a succeeded or failed event (#205)
    * fix: send failure events when we cannot send a succeeded or failed payload
    
    * chore: lint

--- hatchet_sdk/clients/dispatcher/dispatcher.py
+++ hatchet_sdk/clients/dispatcher/dispatcher.py
@@ -8,6 +8,8 @@ from hatchet_sdk.clients.dispatcher.action_listener import (
 from hatchet_sdk.clients.rest.tenacity_utils import tenacity_retry
 from hatchet_sdk.connection import new_conn
 from hatchet_sdk.contracts.dispatcher_pb2 import (
+    STEP_EVENT_TYPE_COMPLETED,
+    STEP_EVENT_TYPE_FAILED,
     ActionEventResponse,
     GroupKeyActionEvent,
     GroupKeyActionEventType,
@@ -63,8 +65,27 @@ class DispatcherClient:
 
         return ActionListener(self.config, response.workerId)
 
-    @tenacity_retry
     async def send_step_action_event(
+        self, action: Action, event_type: StepActionEventType, payload: str
+    ):
+        try:
+            return await self._try_send_step_action_event(action, event_type, payload)
+        except Exception as e:
+            # for step action events, send a failure event when we cannot send the completed event
+            if (
+                event_type == STEP_EVENT_TYPE_COMPLETED
+                or event_type == STEP_EVENT_TYPE_FAILED
+            ):
+                await self._try_send_step_action_event(
+                    action,
+                    STEP_EVENT_TYPE_FAILED,
+                    "Failed to send finished event: " + str(e),
+                )
+
+            return
+
+    @tenacity_retry
+    async def _try_send_step_action_event(
         self, action: Action, event_type: StepActionEventType, payload: str
     ):
         eventTimestamp = Timestamp()
