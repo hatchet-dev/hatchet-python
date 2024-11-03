6d37bebe4bf305b23cb4f478638c0a027ed1a66d
Matt Kaye <mrkaye97@gmail.com>
2024-11-03 14:10:25 -0500

    swallow warning at tracer init

--- hatchet_sdk/utils/tracing.py
+++ hatchet_sdk/utils/tracing.py
@@ -1,6 +1,6 @@
 from opentelemetry import trace
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace import TracerProvider, Tracer
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
 )
@@ -14,13 +14,15 @@ from functools import cache
 class OTelTracingFactory:
     @classmethod
     @cache
-    def create_tracer(cls, name: str, config: ClientConfig):
+    def create_tracer(cls, name: str, config: ClientConfig) -> Tracer:
         ## TODO: Figure out how to specify protocol here
-        resource = Resource(attributes={SERVICE_NAME: config.otel_service_name})
+        resource = Resource(
+            attributes={SERVICE_NAME: config.otel_service_name or "hatchet.run"}
+        )
         processor = BatchSpanProcessor(
             OTLPSpanExporter(
                 endpoint=config.otel_exporter_oltp_endpoint,
-                headers=config.otel_exporter_oltp_headers
+                headers=config.otel_exporter_oltp_headers,
             ),
         )
 
