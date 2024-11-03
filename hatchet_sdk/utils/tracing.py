5c4daca420e68e8d7c6bc1afa1f57377f335f2cd
Matt Kaye <mrkaye97@gmail.com>
2024-11-03 15:53:10 -0500

    add utilities for getting and setting context to tracing service

--- hatchet_sdk/utils/tracing.py
+++ hatchet_sdk/utils/tracing.py
@@ -1,18 +1,22 @@
+import json
+
 from opentelemetry import trace
+from opentelemetry.context import Context
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider, Tracer
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
 )
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
+from typing import Any
 
 from hatchet_sdk.loader import ClientConfig
 from functools import cache
+from hatchet_sdk.utils.serialization import flatten
 
-
-class OTelTracingFactory:
-    @classmethod
+class TracingService:
     @cache
     def create_tracer(cls, name: str, config: ClientConfig) -> Tracer:
         ## TODO: Figure out how to specify protocol here
@@ -32,3 +36,28 @@ class OTelTracingFactory:
         trace.set_tracer_provider(trace_provider)
 
         return trace.get_tracer(name)
+
+
+    def __init__(self, name: str, config: ClientConfig):
+        self.tracer = self.create_tracer(name, config)
+
+    def munge_metadata(self, meta: dict[str, Any] | None) -> bytes | None:
+        span = trace.get_current_span()
+        span.set_attributes(flatten(meta, parent_key="", separator="."))
+
+        carrier = {}
+        TraceContextTextMapPropagator().inject(carrier)
+
+        meta["__otel_carrier"] = carrier
+
+        return None if meta is None else json.dumps(meta).encode("utf-8")
+
+    def parse_carrier_from_metadata(self, metadata: dict[str, Any] | None) -> Context:
+        metadata |= {}
+
+        return (
+            TraceContextTextMapPropagator().extract(_ctx)
+            if (_ctx := metadata.get("__otel_carrier"))
+            else None
+        )
+
