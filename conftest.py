5b030796c09464753914a59eecc6c649bc47761b
Matt Kaye <mrkaye97@gmail.com>
2024-11-05 17:57:57 -0500

    linters

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
+from typing import AsyncGenerator
+
 import pytest
 import pytest_asyncio
-
-from typing import AsyncGenerator
 
 from hatchet_sdk import Hatchet
 
@@ -9,6 +9,7 @@ from hatchet_sdk import Hatchet
 @pytest_asyncio.fixture(scope="session")
 async def aiohatchet() -> AsyncGenerator[Hatchet, None]:
     yield Hatchet(debug=True)
+
 
 @pytest.fixture(scope="session")
 def hatchet() -> Hatchet:
