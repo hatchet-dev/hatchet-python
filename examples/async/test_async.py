68b700dc9d0169c765c1511793d202cde161e71d
hatchet-temporary <mrkaye97+hatchet@gmail.com>
2024-11-21 20:15:12 -0500

    Revert "fix: try commenting entire test out"
    This reverts commit adb06f709c8d770965c09ce351320a9cf6fd1248.

--- examples/async/test_async.py
+++ examples/async/test_async.py
@@ -1,23 +1,23 @@
-# import pytest
+import pytest
 
-# from hatchet_sdk import Hatchet
-# from tests.utils import fixture_bg_worker
+from hatchet_sdk import Hatchet
+from tests.utils import fixture_bg_worker
 
-# # worker = fixture_bg_worker(["poetry", "run", "async"])
-
-
-# # requires scope module or higher for shared event loop
-# @pytest.mark.skip(reason="Skipping this test until we can dedicate more time to debug")
-# @pytest.mark.asyncio(scope="session")
-# async def test_run(hatchet: Hatchet):
-#     run = hatchet.admin.run_workflow("AsyncWorkflow", {})
-#     result = await run.result()
-#     assert result["step1"]["test"] == "test"
+# worker = fixture_bg_worker(["poetry", "run", "async"])
 
 
-# @pytest.mark.skip(reason="Skipping this test until we can dedicate more time to debug")
-# @pytest.mark.asyncio(scope="session")
-# async def test_run_async(aiohatchet: Hatchet):
-#     run = await aiohatchet.admin.aio.run_workflow("AsyncWorkflow", {})
-#     result = await run.result()
-#     assert result["step1"]["test"] == "test"
+# requires scope module or higher for shared event loop
+@pytest.mark.skip(reason="Skipping this test until we can dedicate more time to debug")
+@pytest.mark.asyncio(scope="session")
+async def test_run(hatchet: Hatchet):
+    run = hatchet.admin.run_workflow("AsyncWorkflow", {})
+    result = await run.result()
+    assert result["step1"]["test"] == "test"
+
+
+@pytest.mark.skip(reason="Skipping this test until we can dedicate more time to debug")
+@pytest.mark.asyncio(scope="session")
+async def test_run_async(aiohatchet: Hatchet):
+    run = await aiohatchet.admin.aio.run_workflow("AsyncWorkflow", {})
+    result = await run.result()
+    assert result["step1"]["test"] == "test"
