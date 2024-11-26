642649ea7237f8dbb096223088c21adf1cc8838d
hatchet-temporary <mrkaye97+hatchet@gmail.com>
2024-11-26 08:40:53 -0500

    feat: use testcontainer for test

--- examples/async/test_async.py
+++ examples/async/test_async.py
@@ -1,15 +1,11 @@
 import pytest
 
 from hatchet_sdk import Hatchet
-from tests.utils import fixture_bg_worker
-
-worker = fixture_bg_worker(["poetry", "run", "async"])
 
 
 # requires scope module or higher for shared event loop
-@pytest.mark.skip(reason="Skipping this test until we can dedicate more time to debug")
 @pytest.mark.asyncio(scope="session")
-async def test_run(hatchet: Hatchet):
+async def test_run(hatchet: Hatchet, worker):
     run = hatchet.admin.run_workflow("AsyncWorkflow", {})
     result = await run.result()
     assert result["step1"]["test"] == "test"
