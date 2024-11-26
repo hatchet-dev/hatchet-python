1e729b43ff0171d819729ff37c7f3a97c5bad8d8
hatchet-temporary <mrkaye97+hatchet@gmail.com>
2024-11-26 08:40:16 -0500

    feat: add testcontainer fixture

--- conftest.py
+++ conftest.py
@@ -1,7 +1,11 @@
-from typing import AsyncGenerator
+import os
+from typing import AsyncGenerator, Generator
 
 import pytest
 import pytest_asyncio
+from testcontainers.core.container import DockerContainer
+from testcontainers.core.image import DockerImage
+from testcontainers.core.waiting_utils import wait_for_logs
 
 from hatchet_sdk import Hatchet
 
@@ -14,3 +18,14 @@ async def aiohatchet() -> AsyncGenerator[Hatchet, None]:
 @pytest.fixture(scope="session")
 def hatchet() -> Hatchet:
     return Hatchet(debug=True)
+
+
+@pytest.fixture(scope="session")
+def worker() -> Generator[DockerContainer, None, None]:
+    with DockerImage(path=".", tag="test-container:latest") as image:
+        with DockerContainer(str(image)).with_env(
+            "HATCHET_CLIENT_TOKEN", os.getenv("HATCHET_CLIENT_TOKEN")
+        ) as container:
+            delay = wait_for_logs(container, "sending heartbeat", timeout=30)
+
+            yield container
