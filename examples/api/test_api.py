6e239e89adb4b1dcfafb0d1e837c7dbcfd355564
hatchet-temporary <mrkaye97+hatchet@gmail.com>
2024-11-27 16:48:38 -0500

    fix: make sure workflows exist

--- examples/api/test_api.py
+++ examples/api/test_api.py
@@ -1,19 +1,21 @@
 import pytest
 
 from hatchet_sdk import Hatchet
+from tests.utils import fixture_bg_worker
+
+worker = fixture_bg_worker(["poetry", "run", "concurrency_limit_rr"])
+
+# requires scope module or higher for shared event loop
+@pytest.mark.asyncio(scope="session")
+async def test_list_workflows(hatchet: Hatchet, worker):
+    workflows = hatchet.rest.workflow_list()
+
+    assert len(workflows.rows) != 0
 
 
 # requires scope module or higher for shared event loop
 @pytest.mark.asyncio(scope="session")
-async def test_list_workflows(hatchet: Hatchet):
-    list = hatchet.rest.workflow_list()
+async def test_async_list_workflows(aiohatchet: Hatchet, worker):
+    workflows = await aiohatchet.rest.aio.workflow_list()
 
-    assert len(list.rows) != 0
-
-
-# requires scope module or higher for shared event loop
-@pytest.mark.asyncio(scope="session")
-async def test_async_list_workflows(aiohatchet: Hatchet):
-    list = await aiohatchet.rest.aio.workflow_list()
-
-    assert len(list.rows) != 0
+    assert len(workflows.rows) != 0
