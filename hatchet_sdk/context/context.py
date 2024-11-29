0ece2b41afbe18d2eba863d2158ea6c17f0a9088
hatchet-temporary <mrkaye97+hatchet@gmail.com>
2024-11-29 08:34:25 -0500

    fix: types for config, etc.

--- hatchet_sdk/context/context.py
+++ hatchet_sdk/context/context.py
@@ -70,6 +70,7 @@ class BaseContext:
             "child_index": self.spawn_index,
             "additional_metadata": meta,
             "desired_worker_id": desired_worker_id,
+            "namespace": None,
         }
 
         self.spawn_index += 1
