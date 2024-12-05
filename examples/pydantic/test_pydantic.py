949cfcf7c3d70db3bd79a5996d0e16630c93ea21
hatchet-temporary <mrkaye97+hatchet@gmail.com>
2024-12-05 18:33:19 -0500

    feat: expand tests a little

--- examples/pydantic/test_pydantic.py
+++ examples/pydantic/test_pydantic.py
@@ -6,10 +6,22 @@ from hatchet_sdk import Hatchet
 # requires scope module or higher for shared event loop
 @pytest.mark.asyncio(scope="session")
 @pytest.mark.parametrize("worker", ["pydantic"], indirect=True)
-async def test_run(hatchet: Hatchet, worker):
+async def test_run_validation_error(hatchet: Hatchet, worker):
     run = hatchet.admin.run_workflow(
         "Parent",
         {},
+    )
+
+    with pytest.raises(Exception, match="1 validation error for ParentInput"):
+        await run.result()
+
+# requires scope module or higher for shared event loop
+@pytest.mark.asyncio(scope="session")
+@pytest.mark.parametrize("worker", ["pydantic"], indirect=True)
+async def test_run(hatchet: Hatchet, worker):
+    run = hatchet.admin.run_workflow(
+        "Parent",
+        {"x": "foobar"},
     )
 
     result = await run.result()
