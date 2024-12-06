882eaa91f7eca492d3e81940106ff2f298f0ca8d
hatchet-temporary <mrkaye97+hatchet@gmail.com>
2024-12-06 11:53:48 -0500

    feat: documentation updates for Pydantic

--- examples/pydantic/worker.py
+++ examples/pydantic/worker.py
@@ -10,24 +10,21 @@ load_dotenv()
 hatchet = Hatchet(debug=True)
 
 
+# ❓ Pydantic
+# This workflow shows example usage of Pydantic within Hatchet
 class ParentInput(BaseModel):
     x: str
-
-
-def pretty_print(tag: str, model: BaseModel) -> None:
-    print()
-    print(tag, model, "\nType:", type(model))  ## This is an instance of `ChildInput`
-    print()
 
 
 @hatchet.workflow(input_validator=ParentInput)
 class Parent:
     @hatchet.step(timeout="5m")
     async def spawn(self, context: Context):
-        ## Cast your `workflow_input` to the type of your `input_validator`
-        input = cast(ParentInput, context.workflow_input())
+        # 👀 this step will always raise an exception
 
-        pretty_print("Workflow Input:", input)  ## This is a `ParentInput`
+        ## Use `typing.cast` to cast your `workflow_input`
+        ## to the type of your `input_validator`
+        input = cast(ParentInput, context.workflow_input())  ## This is a `ParentInput`
 
         child = await context.aio.spawn_workflow(
             "Child",
@@ -50,27 +47,30 @@ class StepResponse(BaseModel):
 class Child:
     @hatchet.step()
     def process(self, context: Context) -> StepResponse:
+        ## This is an instance `ChildInput`
         input = cast(ChildInput, context.workflow_input())
-
-        pretty_print("Workflow Input:", input)  ## This is a `ChildInput`
 
         return StepResponse(status="success")
 
     @hatchet.step(parents=["process"])
     def process2(self, context: Context) -> StepResponse:
+        ## This is an instance of `StepResponse`
         process_output = cast(StepResponse, context.step_output("process"))
-
-        pretty_print("Process Output:", process_output)  ## This is a `StepResponse`
 
         return {"status": "step 2 - success"}
 
     @hatchet.step(parents=["process2"])
     def process3(self, context: Context) -> StepResponse:
+        ## This is an instance of `StepResponse`, even though the
+        ## response of `process2` was a dictionary. Note that
+        ## Hatchet will attempt to parse that dictionary into
+        ## an object of type `StepResponse`
         process_2_output = cast(StepResponse, context.step_output("process2"))
 
-        pretty_print("Process 2 Output:", process_2_output)  ## This is a `StepResponse`
-
         return StepResponse(status="step 3 - success")
+
+
+# ‼️
 
 
 def main():
