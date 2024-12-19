592d2167567425c9028f7d528b1b0ec365a3f633
hatchet-temporary <mrkaye97+hatchet@gmail.com>
2024-12-18 21:24:52 -0500

    fix: unwind

--- hatchet_sdk/worker/runner/utils/error_with_traceback.py
+++ hatchet_sdk/worker/runner/utils/error_with_traceback.py
@@ -1,6 +1,6 @@
 import traceback
 
 
-def error_with_traceback(message: str, e: Exception) -> str:
+def errorWithTraceback(message: str, e: Exception) -> str:
     trace = "".join(traceback.format_exception(type(e), e, e.__traceback__))
     return f"{message}\n{trace}"
