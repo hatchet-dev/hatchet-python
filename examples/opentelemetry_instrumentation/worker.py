86086d2de51a7446399f4a8aa6789a4108e5e8d8
mrkaye97 <mrkaye97@gmail.com>
2025-02-06 20:52:23 -0700

    feat: improve example more

--- examples/opentelemetry_instrumentation/worker.py
+++ examples/opentelemetry_instrumentation/worker.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-from opentelemetry import trace
 
 from examples.opentelemetry_instrumentation.tracer import trace_provider
 from hatchet_sdk import Context, Hatchet
@@ -17,17 +16,30 @@ HatchetInstrumentor().instrument(
 @hatchet.workflow(on_events=["otel:event"])
 class OTelWorkflow:
     @hatchet.step()
-    def step1(self, context: Context) -> dict[str, str]:
+    def your_spans_are_children_of_hatchet_span(
+        self, context: Context
+    ) -> dict[str, str]:
         with trace_provider.get_tracer(__name__).start_as_current_span("step1"):
-            print("executed step1")
+            print("executed step")
             return {
-                "step1": "step1",
+                "foo": "bar",
             }
 
     @hatchet.step()
-    def step2(self, context: Context) -> None:
+    def your_spans_are_still_children_of_hatchet_span(self, context: Context) -> None:
         with trace_provider.get_tracer(__name__).start_as_current_span("step2"):
-            raise Exception("step 2 failed")
+            raise Exception("Manually instrumented step failed failed")
+
+    @hatchet.step()
+    def this_step_is_still_instrumented(self, context: Context) -> dict[str, str]:
+        print("executed still-instrumented step")
+        return {
+            "still": "instrumented",
+        }
+
+    @hatchet.step()
+    def this_step_is_also_still_instrumented(self, context: Context) -> None:
+        raise Exception("Still-instrumented step failed")
 
 
 def main() -> None:
