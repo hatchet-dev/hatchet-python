384ca1152b1c0a1b699e1a1d957ea5ab513ea81d
mrkaye97 <mrkaye97@gmail.com>
2025-02-07 21:20:32 -0700

    fix: hack for CI

--- examples/opentelemetry_instrumentation/tracer.py
+++ examples/opentelemetry_instrumentation/tracer.py
@@ -5,36 +5,41 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace import NoOpTracerProvider
 
-from examples.opentelemetry_instrumentation.client import hatchet
+trace_provider: TracerProvider | NoOpTracerProvider
 
-resource = Resource(
-    attributes={
-        SERVICE_NAME: os.getenv("HATCHET_CLIENT_OTEL_SERVICE_NAME", "test-service")
-    }
-)
+if os.getenv("CI", "false") == "true":
+    trace_provider = NoOpTracerProvider()
+else:
+    resource = Resource(
+        attributes={
+            SERVICE_NAME: os.getenv("HATCHET_CLIENT_OTEL_SERVICE_NAME", "test-service")
+        }
+    )
 
-headers = dict(
-    [
-        cast(
-            tuple[str, str],
-            tuple(
-                os.getenv("HATCHET_CLIENT_OTEL_EXPORTER_OTLP_HEADERS", "foo=bar").split(
-                    "="
-                )
+    headers = dict(
+        [
+            cast(
+                tuple[str, str],
+                tuple(
+                    os.getenv(
+                        "HATCHET_CLIENT_OTEL_EXPORTER_OTLP_HEADERS", "foo=bar"
+                    ).split("=")
+                ),
+            )
+        ]
+    )
+
+    processor = BatchSpanProcessor(
+        OTLPSpanExporter(
+            endpoint=os.getenv(
+                "HATCHET_CLIENT_OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317"
             ),
-        )
-    ]
-)
-
-processor = BatchSpanProcessor(
-    OTLPSpanExporter(
-        endpoint=os.getenv(
-            "HATCHET_CLIENT_OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317"
+            headers=headers,
         ),
-        headers=headers,
-    ),
-)
+    )
 
-trace_provider = TracerProvider(resource=resource)
-trace_provider.add_span_processor(processor)
+    trace_provider = TracerProvider(resource=resource)
+
+    trace_provider.add_span_processor(processor)
