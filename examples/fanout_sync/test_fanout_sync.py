47d86a5691887c20b594b57fdd8d584314f242e1
mrkaye97 <mrkaye97@gmail.com>
2025-02-18 10:37:50 -0500

    fix: cruft

--- examples/fanout_sync/test_fanout_sync.py
+++ examples/fanout_sync/test_fanout_sync.py
@@ -3,7 +3,6 @@ import pytest
 from hatchet_sdk import Hatchet, Worker
 
 
-# requires scope module or higher for shared event loop
 @pytest.mark.parametrize("worker", ["fanout_sync"], indirect=True)
 def test_run(hatchet: Hatchet, worker: Worker) -> None:
     run = hatchet.admin.run_workflow("SyncFanoutParent", {"n": 2})
